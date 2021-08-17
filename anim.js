e749ae7c919aa76f3326626488c67fb032ce3711
devanshi <devanshi2122002@gmail.com>
2021-08-18 04:17:01 +0530

    Back to top button added for NMO pages

--- anim.js
+++ anim.js
@@ -15,15 +15,40 @@ function displayQuote(){
 
 
 /****************Text animation ************************/
+jQuery(window).scroll(function() {
+    if (jQuery(window).scrollTop() < 50) {
+        jQuery('#rocketmeluncur').slideUp(500);
+    } else {
+        jQuery('#rocketmeluncur').slideDown(500);
+    }
+    var ftrocketmeluncur = jQuery("#ft")[0] ? jQuery("#ft")[0] : jQuery(document.body)[0];
+    var scrolltoprocketmeluncur = $('rocketmeluncur');
+    var viewPortHeightrocketmeluncur = parseInt(document.documentElement.clientHeight);
+    var scrollHeightrocketmeluncur = parseInt(document.body.getBoundingClientRect().top);
+    var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
+    var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
+    if (basewrocketmeluncur < 1000) {
+        var leftrocketmeluncur = parseInt(ftrocketmeluncur.offsetLeft);
+        leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
+        scrolltoprocketmeluncur.css('left', (basewrocketmeluncur + leftrocketmeluncur + "px"));
+    } else {
+        scrolltoprocketmeluncur.css('left', 'auto');
+        scrolltoprocketmeluncur.css('right', '10px');
+    }
+});
 
-var text1 = ["I'm a", "I'm"];
-var text2 = ["\"Software Developer\"","\"into amazing experience\""];
-var count = 2;
-setInterval(() => {
-  count--;
-  document.getElementById('text-1').innerHTML= text1[count];
-  document.getElementById('text-2-span').innerHTML= text2[count];
-  if(count == 0){
-    count = 2;
-  }
-}, 7000);
+jQuery('#rocketmeluncur').click(function() {
+    jQuery("html, body").animate({
+        scrollTop: '0px',
+        display: 'none'
+    }, {
+        duration: 600,
+        easing: 'linear'
+    });
+
+    var self = this;
+    this.className += ' ' + "launchrocket";
+    setTimeout(function() {
+        self.className = 'showrocket';
+    }, 800);
+});
