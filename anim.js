390c74382795845d933e2cc1dd9a2e9e4dce7261
devanshi <devanshi2122002@gmail.com>
2021-08-20 04:59:55 +0530

    Added years of exp and TODO - Companies

--- anim.js
+++ anim.js
@@ -113,3 +113,21 @@ window.onload = function() {
 
 
 /***********************Tey Photo gallary */
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 3, 1000);
+   });
+   
