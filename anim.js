f8af8cd69c1148dda852986a71b058b13c1ce212
devanshi <devanshi2122002@gmail.com>
2021-08-22 00:54:22 +0530

    Index(icons), All Pages nav bar, Comments added for each setion

--- anim.js
+++ anim.js
@@ -112,7 +112,7 @@ window.onload = function() {
 };
 
 
-/***********************Tey Photo gallary */
+/*********************** Photo gallary **************************/
 document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration) {
      let obj = document.getElementById(id),
