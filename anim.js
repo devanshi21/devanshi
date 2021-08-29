dfebb2359ef7959601d815c5f4a84a4087b064de
devanshi <devanshi2122002@gmail.com>
2021-08-29 17:20:24 +0530

    Awards timeline added

--- anim.js
+++ anim.js
@@ -131,5 +131,50 @@ document.addEventListener("DOMContentLoaded", () => {
     counter("count1", 0, 3, 1000);
    });
 
-   /******************Cars slidder********************************/
+   /******************Awards animation********************************/
+   $(function(){
+    window.sr = ScrollReveal();
+    if ($(window).width() < 768) {
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
   
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    } else {
+        
+        sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  });
